1ea99d77b8b92bcbcad40de8cd96e79a75a4322a
Raghuvansh Agarwal <rarchva@gmail.com>
2022-10-21 19:12:40 +0530

    Code refactored

--- main.cpp
+++ main.cpp
@@ -2,50 +2,38 @@
 #include "Movies.h"
 #include "Utility.h"
 
+void init_add_movie(Movies& movies);
+void init_watch_time_increment(Movies& movies);
 void add_movie(Movies& movies, const std::string& name,const std::string& rating,size_t watch_time);
 void increment_watched(Movies& movies, const std::string& name);
 
 int main() {
-    Movies* my_movies = new Movies();
+    auto* my_movies = new Movies();
     bool should_continue {true};
 
     int choice;
 
     while(should_continue){
-        choice = Utility::take_choice(std::vector<std::string>{"Add Movie","Increment Watch Count for Movie","Exit"});
+        choice = Utility::take_choice(std::vector<std::string>{"Add Movie","Increment Watch Count for Movie", "Print All Movies" ,"Exit"});
         switch (choice) {
             case 1:
-                std::string name;
-                std::string rating;
-                int watch_time;
-
-                std::cout<<"Name of the Movie => ";
-                std::getline(std::cin,name);
-
-                std::vector<std::string> options {"Add Movie","Increment Watch Count for Movie","Exit"};
-
-                bool valid_option_taken {false};
-                while (!valid_option_taken){
-                    int rate = Utility::take_choice(options);
-                    if(rate < options.size()){
-                        rating = options[rate];
-                        valid_option_taken = true;
-                    }else{
-                        std::cout<<"Invalid Rating..." <<std::endl;
-                    }
-                }
+                init_add_movie(*my_movies);
                 break;
             case 2:
-                std::cout << "Invalid Input Try Again" << std::endl;
+                init_watch_time_increment(*my_movies);
                 break;
             case 3:
+                my_movies->print_movies();
+                break;
+            case 4:
                 should_continue = false;
-                std::cout << "Invalid Input Try Again" << std::endl;
+                std::cout << "Program Exit......" << std::endl;
                 break;
             default:
                 std::cout << "Invalid Input Try Again" << std::endl;
                 break;
         }
+        Utility::print_breaker();
     }
 
 
@@ -67,3 +55,40 @@ void increment_watched(Movies& movies, const std::string& name){
     }
 }
 
+void init_add_movie(Movies& movies){
+    std::string name;
+    std::string rating;
+    size_t watch_time;
+
+    std::cin.ignore();
+    std::cout<<"Name of the Movie => ";
+    std::getline(std::cin,name);
+
+
+    std::vector<std::string> options {"G","PG","PG-13","R"};
+
+    bool valid_option_taken {false};
+    while (!valid_option_taken){
+        int rate = Utility::take_choice(options);
+        if(rate < static_cast<int>(options.size())){
+            rating = options[rate];
+            valid_option_taken = true;
+        }else{
+            std::cout<<"Invalid Rating..." <<std::endl;
+        }
+    }
+    std::cout<<"Number of Times Movie Watched => ";
+    std::cin >> watch_time;
+
+    add_movie(movies,name,rating,watch_time);
+}
+
+void init_watch_time_increment(Movies& movies){
+    std::string name;
+
+    std::cin.clear();
+    std::cout<<"Name of the Movie => ";
+    std::getline(std::cin,name);
+
+    increment_watched(movies,name);
+}
